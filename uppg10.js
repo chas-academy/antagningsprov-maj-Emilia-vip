590152c1c3064268fd6d757debe36300d3606576
Emilia-vip <emiliaviperstrand@gmail.com>
2025-05-08 15:28:30 +0200

    Update uppg10.js

--- uppg10.js
+++ uppg10.js
@@ -1,17 +1,23 @@
 function uppg10(){
     const numbers = [5, 12, 7, 10, 20, 9, 11];
-  
-    // skapa tre arrayer - bigEven, smallOdd, other
-    // loopa igenom numbers och placera talen i respektive array
-    // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
-    // smallOdd ska innehålla tal mindre än 10 och udda
-    // other ska innehålla alla andra tal
-    // returnera ett objekt med de tre arrayerna
+const bigEven = [];
+const smallOddm = [];
+const other = [];
 
-  
-    
-  
-  }
+    for (let num of numbers) {
+        if (num > 10 && num % 2 ===0) {
+            bigEven.push(num);
+        } else if (num < 10 && num % 2 !==0 {
+            smallOdd.push(num);
+        } else{
+            other.push(num);
+        }
+return {
+    bigEven,
+    smallOdd,
+    other
+  };
+}
   
   module.exports = { uppg10 };
   
