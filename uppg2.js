1ca3f26093f3f3ec36632a68f182a4c20551144c
Emilia-vip <emiliaviperstrand@gmail.com>
2025-05-08 16:00:51 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -1,9 +1,7 @@
 
 function uppg2() {
-   // Skriv din kod här
-   // deklarera en variable med ditt namn och skriv ut det i konsolen
-
-
+const namn = "Emilia";
+   console.log(namn);
 }
 
 module.exports = { uppg2 };
