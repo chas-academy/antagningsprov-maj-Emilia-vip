bbb83e724405efca7b823f1d7d46f9973288c067
Emilia-vip <emiliaviperstrand@gmail.com>
2025-05-08 17:20:28 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -1,17 +1,21 @@
 
 
 function uppg8(){
+const personer = [
+  {name: "Emilia", age: 21},
+  {name: "Filip", age: 25},
+  {name: "Jeanette", age: 31},
+  {name: "Frank", age: 29},'
+  {name: "Ellen", age: 36},
+  ];
 
-// skapa en array med fem person obkjet
-// varje person objekt ska ha två properties, name och age
-
-// skapa en fuktion som tar in en array som argument
-// skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
-//  över 30 år som skrivs ut i konsolen
-
-// anropa funktionen och skicka med arrayen som argument
-
-
+  funktion VisaPersonerOver30(personArray){
+    for (let person of personArray){
+      if (person.age > 30) {
+        console.log(person.name);
+      }
+   }
 }
-
+VisaPersonerOver30(personer);
+}
 module.exports = { uppg8 };
