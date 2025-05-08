b32459a88321a383719ede6e5fa8780107804ffd
Emilia-vip <emiliaviperstrand@gmail.com>
2025-05-08 17:35:59 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -1,11 +1,13 @@
 
 function uppg5() {
-    // skriv en if-sats som jämför två tal
-    // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
-    // annars skriv ut "Det andra talet är större än det första"
+let a = 7;
+let b = 12;
 
-
-
+if (a>b) {
+    console.log("Det första talet är större än det andra");
+} else{
+    console.log("Det andra talet är större än det första");
+  }
 }
 
 module.exports = { uppg5 };
