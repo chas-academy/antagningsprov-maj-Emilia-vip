9c06e49224dd4975e5c5d3aba60fe69c04b3da82
Emilia-vip <emiliaviperstrand@gmail.com>
2025-05-08 15:36:25 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -1,14 +1,15 @@
 
 
 function uppg9 (){
-    // skapa en funktion som heter sort och tar in en array av siffror som argument
-    // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
-    // i if-sattsen skriv ut i konsolen resultatet
-    // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
+const numbers = [4,7,10,13,22,5];
 
-    // anropa funktionen och skicka med en array som argument
-
-  
+for (let num of numbers) {
+    if (num % 2 === 0) {
+        console.log(num + "jämt");
+    } else {
+        console.log(num + "udda");
+    }
+  } 
 }
 
 module.exports = { uppg9 };
